50c5c3ba06942752dad9795b6cbb247dc31f44d1
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-14 00:15:38 -0700

    Update streamlit_app.py
    use library abbreviations

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 
 st.header('🍌 Build Your Own Fruit Smoothie 🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
 
