d8a34c5210444fb79add97cc97b5d6435729b20c
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-13 21:30:45 -0700

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Hello World...')
-st.header('My name is: first_streamlit_app')
-st.text('I am the first app designed')
-
-st.text('🥣 🥗 🐔 🥑🍞')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🍜 Omega 3 & Blueberry Oatmeal')
+st.text('🍞 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌 Build Your Own Fruit Smoothie 🍇')
