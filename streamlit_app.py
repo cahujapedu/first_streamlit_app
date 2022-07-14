25f4d6e81a2ab5f64f8231b64d7856d6734c9cac
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-14 00:14:25 -0700

    Update streamlit_app.py
    read fruit_macros csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ st.text('🍞 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 
 st.header('🍌 Build Your Own Fruit Smoothie 🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
