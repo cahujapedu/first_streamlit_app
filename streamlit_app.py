2d18de774ed251f2e2642cf64fe84d8a6c38fa26
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-13 21:17:50 -0700

    Update streamlit_app.py
    Added breakfast/menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ st.title('Hello World...')
 st.header('My name is: first_streamlit_app')
 st.text('I am the first app designed')
 
+st.text('🥣 🥗 🐔 🥑🍞')
+
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
