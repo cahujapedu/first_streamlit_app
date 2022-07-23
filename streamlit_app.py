e119539c4c699ace5b50eecff2dedc2757a30687
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-23 15:27:40 -0700

    Update streamlit_app.py
    use library abbreviation

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ st.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
