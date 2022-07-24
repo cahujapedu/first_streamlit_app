02fad6edc9f33043063cb3f094c9c38d116fc85b
Chander Ahuja <109047231+cahujapedu@users.noreply.github.com>
2022-07-23 18:40:35 -0700

    Update streamlit_app.py
    restore code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,6 @@ fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
-  ##st.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
